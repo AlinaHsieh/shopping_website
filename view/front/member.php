0e1f4820e63e1cdcf40f9d957f3b23415a77f33b
Alina <ahsieh74@gmail.com>
2023-09-20 19:47:25 +0800

    建構會員中心:會員可檢視會員資訊及歷史訂購資訊

--- view/front/member.php
+++ view/front/member.php
@@ -1,11 +1,71 @@
+<?php
+$rows = $Order->all(['acc' => $_SESSION['user']]);
+$user = $User->find(['acc'=>$_SESSION['user']]);
+?>
+
 <h2>會員中心</h2>
+
 <table class="all">
     <tr>
-        <td>訂單編號</td>
-        <td></td>
+        <td class="tt" colspan="2">會員資料</td>
     </tr>
     <tr>
-        <td>訂購日期</td>
-        <td></td>
+        <td class="pp">帳號名稱</td>
+        <td class="pp"><?=$user['acc']?></a></td>
+        
     </tr>
+    <tr>
+
+        <td class="pp">姓名</td>
+        <td class="pp"><?=$user['name']?></td>
+        
+    </tr>
+    <tr>
+
+        <td class="pp">email</td>
+        <td class="pp"><?=$user['email']?></td>
+
+    </tr>
+    <tr>
+
+        <td class="pp">電話</td>
+        <td class="pp"><?=$user['tel']?></td>
+
+    </tr>
+    <tr>
+        <td class="pp">地址</td>
+        <td class="pp"><?=$user['addr']?></td>
+
+    </tr>
+    <?php
+    if (isset($rows)) {
+        foreach ($rows as $row) {
+    ?>
+
+    <?php
+        }
+    }
+    ?>
+</table>
+
+<table class="all">
+    <tr>
+        <td class="tt" colspan="2">歷史訂單</td>
+    </tr>
+    <tr>
+        <td class="pp">訂單編號</td>
+        <td class="pp">訂購日期</td>
+    </tr>
+    <?php
+    if (isset($rows)) {
+        foreach ($rows as $row) {
+    ?>
+            <tr>
+                <td class="pp"><a href="?do=order_detail&no=<?=$row['no']?>" style='color:blue'><?=$row['no']?></a></td>
+                <td class="pp"><?=$row['orddate']?></td>
+            </tr>
+    <?php
+        }
+    }
+    ?>
 </table>
